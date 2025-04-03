a4fb3d66441e3285e796c24fc1367c78a1d4a7d9
bkateliya <bkateliya@horizontal.com>
2025-04-03 17:28:04 +0530

    code update

--- hackoholics/src/sxastarter/tailwind.config.js
+++ hackoholics/src/sxastarter/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        colorprimary: 'var(--main-bg-colo9r, #ff0000)',
+        colorprimary: 'var(--main-bg-color, #ff0000)',
       },
     },
   },
